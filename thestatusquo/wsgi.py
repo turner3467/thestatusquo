a4becf006df0e2a25304738c4bbe5e06c1286bd4
Jeremy Turner <turner3467@gmail.com>
2018-02-09 19:51:37 +0000

    Change wsgi to production settings

--- thestatusquo/wsgi.py
+++ thestatusquo/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thestatusquo.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thestatusquo.settings.production")
 
 application = get_wsgi_application()
